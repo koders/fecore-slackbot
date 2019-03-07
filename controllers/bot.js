588dd3485f2a5c3d8792586718d60af962469a08
Rihards Fridrihsons <rfridrihsons@evolutiongaming.com>
2019-03-07 13:18:59 +0200

    Read token from env

--- controllers/bot.js
+++ controllers/bot.js
@@ -1,6 +1,6 @@
 const { WebClient } = require('@slack/client');
 // An access token (from your Slack app or custom integration - xoxp, xoxb)
-const token = "xoxp-2369683355-480328589971-569495721283-1e4bd95c25ebb7f667be849f2faa66af";
+const token = process.env.token;
 
 const web = new WebClient(token);
 
