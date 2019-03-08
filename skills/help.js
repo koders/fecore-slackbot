a9a3596a25366cf6c12aa3f568cda6f05931a914
Rihards Fridrihsons <rfridrihsons@evolutiongaming.com>
2019-03-08 12:45:50 +0200

    Update help

--- skills/help.js
+++ skills/help.js
@@ -2,6 +2,19 @@ const slackApi = require("../api/slack");
 
 module.exports = async (payload) => {
     const { channel } = payload.event;
-    return await slackApi.postMessage(channel, `Here's how I can help you:
-            - @bot 123: does nothing...`);
+    const commands = [
+        {title: "help", description: "Show help"},
+        {title: "td get", description: "Show current true defective"},
+        {title: "td set @user", description: "Set new true defective"},
+        {title: "joke [category]", description: "Brighten day"},
+        {title: "joke categories", description: "Joke list"},
+        {title: "hang list [@user]", description: "Show list of hanged people or hang reasons for a single user"},
+        {title: "hang @user [reason]", description: "Hangs user"},
+        {title: "unhang @user", description: "Removes last hang for user"},
+        {title: "mg", description: "Show status of next grooming"},
+        {title: "mg request reason", description: "Request next grooming"},
+        {title: "mg urgent reason", description: "Request urgent grooming, to be right now"},
+    ];
+    const message = commands.map(command => `*${command.title}* | ${command.description}\n`);
+    return await slackApi.postMessage(channel, "This is the stuff I know:\n" + message.join(""));
 };
---
+++ skills/joke.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+const slackApi = require("../api/slack");
+
+const randomIntros = [
+    "",
+    "",
+    "I've got one for you - ",
+    "You got it, buddy, - ",
+    "Here you go - ",
+    "Catch this - ",
+];
+
+module.exports = async (payload) => {
+    const { channel, text } = payload.event;
+    const commands = text.split(" ");
+    const category = commands[2];
+
+    if (category === "categories") {
+        return getCategories(channel);
+    }
+
+    return getJoke(channel, category);
+};
+
+const getJoke = async (channel, category) => {
+    const categoryLink = category ? `?category=${category}` : "";
+    const response = await axios.get("http://api.chucknorris.io/jokes/random" + categoryLink);
+    const randomNumber = Math.floor(Math.random() * (randomIntros.length - 1));
+    const randomIntro = randomIntros[randomNumber];
+    return await slackApi.postMessage(channel, randomIntro + response.data.value);
+}
+
+const getCategories = async (channel) => {
+    const response = await axios.get("https://api.chucknorris.io/jokes/categories");
+    console.log("categories", response.data);
+    
+    return await slackApi.postMessage(channel, "I've got categories like - " + response.data.join(", "));
+}
